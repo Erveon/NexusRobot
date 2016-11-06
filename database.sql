56a48a8b97e4cb8987562b79880e060f7a8aafa2
veerhoff <averhoef7@gmail.com>
2016-11-06 09:19:13 -0500

    fixed database versions

--- database.sql
+++ database.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS `nexus_twitch`;
 USE `nexus_twitch`;
 
-CREATE TABLE `command_counter` (
-  `command` varchar(20) NOT NULL,
+CREATE TABLE `nude_counter` (
+  `username` varchar(20) NOT NULL,
   `count` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `command_counter` (`command`, `count`) VALUES ('nudes', 0);
+INSERT INTO `nude_counter` (`username`, `count`) VALUES ('sample', 0);
 
-ALTER TABLE `command_counter`
-  ADD PRIMARY KEY (`command`);
+ALTER TABLE `nude_counter`
+  ADD PRIMARY KEY (`username`);
