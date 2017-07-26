5fe409942e776d9cf5de0dcdd30f3d0fe0f90376
veerhoff <averhoef7@gmail.com>
2017-07-25 21:32:57 -0400

    whoa howd database.sql get so fucked up

--- database.sql
+++ database.sql
@@ -1,8 +1,9 @@
+
 CREATE DATABASE IF NOT EXISTS `nexus_twitch`;
 USE `nexus_twitch`;
 
 CREATE TABLE `nude_counter` (
-  `username` varchar(20) NOT NULL,
+  `username` varchar(40) NOT NULL,
   `count` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -11,5 +12,14 @@ INSERT INTO `nude_counter` (`username`, `count`) VALUES ('sample', 0);
 ALTER TABLE `nude_counter`
   ADD PRIMARY KEY (`username`);
 
+
+
 CREATE TABLE `custom_commands` (
-  `command_name` varchar(20) NOT NULL,
+	`command_name` varchar(40) NOT NULL,
+	`command_output` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `custom_commands` (`command_name`,`command_output`) VALUES ('command','output');
+
+ALTER TABLE `custom_commands`
+  ADD PRIMARY KEY (`command_name`);
